a7b2d6d7a91c01337da3af0634510840aba2a06f
kensuke0518 <k.com.play@gmail.com>
2021-04-01 13:03:23 +0900

    グロナビ移動時にheaderのpaddingの値がおかしくなっていたのを修正

--- src/js/gnavScrollFunc.js
+++ src/js/gnavScrollFunc.js
@@ -4,10 +4,11 @@ const mqmd = 599; //ブレークポイント
 export const gnavScrollFunc = () => {
     const header = document.querySelector('.l-header');
     const gnav = document.querySelector('.l-gnav');
-    const headerBottom = header.getBoundingClientRect().bottom;
-    const headerScrollBottom = window.pageYOffset + headerBottom;
     
     const headerPosition = () => {
+        const headerPb = isNaN(parseInt(header.style.paddingBottom)) ? 0 : parseInt(header.style.paddingBottom);
+        const headerBottom = header.getBoundingClientRect().bottom - headerPb;
+        const headerScrollBottom = window.pageYOffset + headerBottom;
         
         const scroll = window.pageYOffset;
         const gnavHeight = gnav.clientHeight;
